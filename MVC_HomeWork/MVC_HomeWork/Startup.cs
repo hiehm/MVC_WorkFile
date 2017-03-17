25dd36c540f8e1b016be7173e20ca3e85592fbcf
MATT <mattchen@infbeyond.com>
2017-03-17 16:02:31 +0800

    original

---
+++ MVC_HomeWork/MVC_HomeWork/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_HomeWork.Startup))]
+namespace MVC_HomeWork
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
